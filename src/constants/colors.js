1009fb3c809eb8e8d362d059ac55ee911cbbbf7a
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-02-15 14:38:33 +0300

    chore: add colors and layout constants

---
+++ src/constants/colors.js
@@ -0,0 +1,10 @@
+export const colors = {
+  primary: '#2F50F4',
+  onPrimary: '#102DBC',
+  secondary: '#FF245B',
+  neutralGray: '#F0F0F5',
+  onNeutralGray: '#D1D4E0',
+  onNeutralGrayText: '#19224C',
+  gray: '#989BB3',
+  background: '#FFFFFF',
+};
---
+++ src/constants/layout.js
@@ -0,0 +1,14 @@
+export const spacing = {
+  extraSmall: 4,
+  bottomTab: 6.5,
+  small: 8,
+  semidefault: 12,
+  default: 16,
+  medium: 24,
+  large: 32,
+};
+
+export const sizes = {
+  navHeader: 64,
+  bottomTab: 55,
+};
