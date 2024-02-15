aba003f213b54b5c64c4778a7f001138433b26a9
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-02-15 14:38:52 +0300

    feat: add native stack navigator

---
+++ src/navigation/MainStackNav.tsx
@@ -0,0 +1,17 @@
+import { Route } from '@customTypes/navigation';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { BenefitScreen } from '@screens/BenefitScreen';
+import { CategoryScreen } from '@screens/CategoryScreen';
+import { MainScreen } from '@screens/MainScreen';
+
+const Stack = createNativeStackNavigator();
+
+export const MainStackNav = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name={Route.Main} component={MainScreen} />
+      <Stack.Screen name={Route.Category} component={CategoryScreen} />
+      <Stack.Screen name={Route.Benefit} component={BenefitScreen} />
+    </Stack.Navigator>
+  );
+};
