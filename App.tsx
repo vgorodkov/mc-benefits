7597ab8d678679a708b457762fcc4b5dd602e6c6
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-02-15 14:40:28 +0300

    feat: add fonts to app, add custom theme

--- App.tsx
+++ App.tsx
@@ -1,20 +1,55 @@
+import 'react-native-gesture-handler';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { BottomTabNav } from 'navigation/BottomTabNav';
+import { colors } from '@constants/colors';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from 'expo-font';
+import { useCallback } from 'react';
+
+SplashScreen.preventAutoHideAsync();
+
+const customTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: colors.background,
+  },
+};
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    'SF-UI-Display-Bold': require('@assets/fonts/SF-UI-Display-Bold.ttf'),
+    'SF-UI-Display-Semibold': require('@assets/fonts/SF-UI-Display-Semibold.ttf'),
+    'SF-UI-Display-Medium': require('@assets/fonts/SF-UI-Display-Medium.ttf'),
+    'SF-UI-Display-Regular': require('@assets/fonts/SF-UI-Display-Regular.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme={customTheme}>
+      <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
+        <StatusBar style="auto" />
+        <BottomTabNav />
+      </SafeAreaView>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
