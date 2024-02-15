e3b5a664399951c2efcb61893fe4d12199594751
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-02-15 14:38:14 +0300

    chore: setup all screens

---
+++ src/screens/BenefitScreen.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+export const BenefitScreen = () => {
+  return (
+    <View>
+      <Text>BenefitScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
---
+++ src/screens/MainScreen.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, View } from 'react-native';
+import React from 'react';
+import { Text } from '@components/common/Text';
+
+export const MainScreen = () => {
+  return (
+    <View>
+      <Text variant="body-sb">MainScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
