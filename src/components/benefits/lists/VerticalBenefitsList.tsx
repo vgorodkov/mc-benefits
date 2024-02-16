f58d31d9d769a40bdbfd4cf73f687ab03fd74d7b
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-02-16 20:51:12 +0300

    refactor: change values to constant spacing

--- src/components/benefits/lists/VerticalBenefitsList.tsx
+++ src/components/benefits/lists/VerticalBenefitsList.tsx
@@ -2,8 +2,10 @@ import { StyleSheet, View } from 'react-native';
 import React from 'react';
 import Animated, { FadeIn, FadeOut, LinearTransition } from 'react-native-reanimated';
 import { Benefit } from '@customTypes/benefits';
-import { BenefitItem } from '@components/benefits/BenefitItem';
+import { BenefitItem } from '@components/benefits/item/BenefitItem';
 import { Text } from '@components/common/Text';
+import { spacing } from '@constants/layout';
+import { animation } from '@constants/animation';
 
 interface VerticalBenefitsList {
   benefitsData: Benefit[];
@@ -14,22 +16,24 @@ export const VerticalBenefitsList = ({ benefitsData, categoryLabel }: VerticalBe
   const renderItem = ({ item, index }: { item: Benefit; index: number }) => {
     return (
       <Animated.View
-        entering={FadeIn.delay(Math.min(index * 300, 900))}
+        entering={FadeIn.delay(
+          Math.min(index * animation.default_duration, animation.default_duration * 3),
+        )}
         exiting={FadeOut}
-        layout={LinearTransition.delay(300)}
+        layout={LinearTransition.delay(animation.default_duration)}
       >
         <BenefitItem benefit={item} variant="detailed" />
       </Animated.View>
     );
   };
   return (
-    <View style={{ paddingVertical: 24, gap: 24, flexGrow: 1 }}>
+    <View style={styles.container}>
       <Text style={styles.verticalBenefitsListTitle} variant="header_1">
         {categoryLabel}
       </Text>
       <Animated.FlatList
         data={benefitsData}
-        itemLayoutAnimation={LinearTransition.delay(300)}
+        itemLayoutAnimation={LinearTransition.delay(animation.default_duration)}
         renderItem={renderItem}
         contentContainerStyle={styles.verticalBenefitsListContent}
         showsVerticalScrollIndicator={false}
@@ -39,13 +43,15 @@ export const VerticalBenefitsList = ({ benefitsData, categoryLabel }: VerticalBe
 };
 
 const styles = StyleSheet.create({
+  container: { paddingVertical: spacing.medium, gap: spacing.medium, flexGrow: 1 },
+
   verticalBenefitsListContent: {
-    gap: 32,
-    paddingHorizontal: 16,
-    paddingBottom: 48,
+    gap: spacing.large,
+    paddingHorizontal: spacing.default,
+    paddingBottom: spacing.medium * 2,
     flexGrow: 1,
   },
   verticalBenefitsListTitle: {
-    paddingLeft: 16,
+    paddingLeft: spacing.default,
   },
 });
