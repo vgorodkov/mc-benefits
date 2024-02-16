8b2f87ee5c91039e41672213be117b3a89e07480
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-02-16 21:07:05 +0300

    style:  AccountScreen styling

--- src/screens/AccountScreen.tsx
+++ src/screens/AccountScreen.tsx
@@ -1,12 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import React from 'react';
+import { Text } from '@components/common/Text';
 
 export const AccountScreen = () => {
   return (
-    <View>
-      <Text>AccountScreen</Text>
+    <View style={styles.container}>
+      <Text variant="subtitle">Account Screen</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
