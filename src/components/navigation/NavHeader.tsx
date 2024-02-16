83b521f51b391c5d67baf49d7f4e2d9f8530d737
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-02-16 15:40:07 +0300

    chore: animation

--- src/components/navigation/NavHeader.tsx
+++ src/components/navigation/NavHeader.tsx
@@ -2,10 +2,11 @@ import { FlatList, StyleSheet, Text, View } from 'react-native';
 import React, { useMemo, useRef } from 'react';
 import { sizes, spacing } from '@constants/layout';
 import { colors } from '@constants/colors';
-import { benefits } from '@components/data/benefits';
+import { benefits } from 'data/benefits';
 import { NavHeaderItem } from '@components/navigation/NavHeaderItem';
 import { useSelector } from 'react-redux';
 import { RootState } from 'redux/store';
+import Animated, { FadeIn, FadeOut } from 'react-native-reanimated';
 
 interface Category {
   id: number;
@@ -15,10 +16,10 @@ interface Category {
 export const NavHeader = () => {
   const categories = useMemo(
     () =>
-      Object.entries(benefits).map((benefit, index) => {
+      benefits.categories.map((category) => {
         return {
-          id: index + 1,
-          label: benefit[0],
+          id: category.id,
+          label: category.title,
         };
       }),
     [],
@@ -44,7 +45,7 @@ export const NavHeader = () => {
   scrollToItem();
 
   return (
-    <View style={styles.navHeaderContainer}>
+    <Animated.View style={styles.navHeaderContainer} exiting={FadeOut} entering={FadeIn}>
       <FlatList
         ref={flatListRef}
         data={categories}
@@ -56,7 +57,7 @@ export const NavHeader = () => {
           <NavHeaderItem id={0} label="Все скидки" isActive={activeCategoryId === 0} />
         )}
       />
-    </View>
+    </Animated.View>
   );
 };
 
