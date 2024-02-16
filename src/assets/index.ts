5e6b7d9c5f2f249e5778b7cb119aad9f4c539364
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-02-16 20:41:21 +0300

    data: imgs map to benefit id

---
+++ src/assets/index.ts
@@ -0,0 +1,51 @@
+interface BenefitsImgs {
+  [key: string]: number;
+}
+
+export const imgs: {
+  benefits: BenefitsImgs;
+  favourite_placeholder: number;
+} = {
+  benefits: {
+    1: require('@assets/imgs/benefits/Odin.png'),
+    2: require('@assets/imgs/benefits/SpiritВar.png'),
+    3: require('@assets/imgs/benefits/Embargo.png'),
+    4: require('@assets/imgs/benefits/Myata.png'),
+    5: require('@assets/imgs/benefits/Dominos.png'),
+    6: require('@assets/imgs/benefits/Evo.png'),
+    7: require('@assets/imgs/benefits/Voka.png'),
+    8: require('@assets/imgs/benefits/Skyeng.png'),
+    9: require('@assets/imgs/benefits/Robolab.png'),
+    10: require('@assets/imgs/benefits/Fasol.png'),
+    11: require('@assets/imgs/benefits/Online.png'),
+    12: require('@assets/imgs/benefits/GoldenApple.png'),
+    13: require('@assets/imgs/benefits/Korean.jpg'),
+    14: require('@assets/imgs/benefits/Beat.png'),
+    15: require('@assets/imgs/benefits/Adidas.jpg'),
+    16: require('@assets/imgs/benefits/Gym.png'),
+    17: require('@assets/imgs/benefits/Hm.png'),
+    18: require('@assets/imgs/benefits/Zara.jpg'),
+    19: require('@assets/imgs/benefits/Moon.jpg'),
+    20: require('@assets/imgs/benefits/Burgerking.png'),
+    21: require('@assets/imgs/benefits/Spotify.png'),
+    22: require('@assets/imgs/benefits/Order.png'),
+    23: require('@assets/imgs/benefits/Billiard.jpg'),
+  },
+  favourite_placeholder: require('@assets/imgs/empty-favourites.png'),
+};
+
+export const icons = {
+  heart: require('@assets/icons/heart.png'),
+  active_heart: require('@assets/icons/active-heart.png'),
+  user: require('@assets/icons/user.png'),
+  active_user: require('@assets/icons/active-user.png'),
+  sale_bar: require('@assets/icons/sale-bar.png'),
+  active_sale_bar: require('@assets/icons/active-sale-bar.png'),
+  active_fire_solid: require('@assets/icons/active-fire-solid.png'),
+  fire_solid: require('@assets/icons/fire-solid.png'),
+  heart_route: require('@assets/icons/heart-route.png'),
+  actitve_heart_route: require('@assets/icons/active-heart-route.png'),
+  arrow_left: require('@assets/icons/arrow-left.png'),
+  search: require('@assets/icons/search.png'),
+  info: require('@assets/icons/info.png'),
+};
