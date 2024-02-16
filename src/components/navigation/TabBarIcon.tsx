c376637ed390d103123b12a212c2219eddf062f6
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-02-16 20:54:41 +0300

    refactor: cache to avoid rerenders on TabBarIcon press

--- src/components/navigation/TabBarIcon.tsx
+++ src/components/navigation/TabBarIcon.tsx
@@ -1,15 +1,14 @@
 import { StyleSheet, Image } from 'react-native';
-import React from 'react';
+import React, { memo } from 'react';
+import { sizes } from '@constants/layout';
 
-const ICON_SIZE = 24;
-
-export const TabBarIcon = ({ img }: { img: number }) => {
+export const TabBarIcon = memo(({ img }: { img: number }) => {
   return <Image source={img} style={styles.tabBarImg} />;
-};
+});
 
 const styles = StyleSheet.create({
   tabBarImg: {
-    width: ICON_SIZE,
-    height: ICON_SIZE,
+    width: sizes.icon,
+    height: sizes.icon,
   },
 });
