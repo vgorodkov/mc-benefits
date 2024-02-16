dedff629e1239cc0e7f5f664287990a0d8dc4f63
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-02-16 15:37:20 +0300

    feat: create category screen

--- src/screens/CategoryScreen.tsx
+++ src/screens/CategoryScreen.tsx
@@ -1,12 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { ActivityIndicator, BackHandler, InteractionManager, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { benefits } from 'data/benefits';
+import benefitsMeta from 'data/benefitsMeta.json';
 
-export const CategoryScreen = () => {
-  return (
-    <View>
-      <Text>CategoryScreen</Text>
-    </View>
+import { useFocusEffect } from '@react-navigation/native';
+import { selectActiveCategory } from 'redux/slices/categorySlice';
+import { useDispatch } from 'react-redux';
+
+import { VerticalBenefitsList } from '@components/benefits/lists/VerticalBenefitsList';
+
+export const CategoryScreen = ({ route }) => {
+  const { categoryLabel, categoryId } = route.params;
+
+  const benefitsByCategory = benefits.categories.find((category) => category.id === categoryId);
+
+  const benefitsData = benefitsMeta.filter((benefit) =>
+    benefitsByCategory?.benefitsIds.includes(benefit.id),
   );
-};
+  const [isLoading, setIsLoading] = useState(true);
+  const dispatch = useDispatch();
 
-const styles = StyleSheet.create({});
+  useEffect(() => {
+    InteractionManager.runAfterInteractions(() => setIsLoading(false));
+  }, []);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      dispatch(selectActiveCategory(categoryId));
+      const onBackPress = () => {
+        dispatch(selectActiveCategory(0));
+        return false;
+      };
+
+      const subscription = BackHandler.addEventListener('hardwareBackPress', onBackPress);
+
+      return () => subscription.remove();
+    }, [categoryId]),
+  );
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size={'large'} />
+      </View>
+    );
+  }
+
+  return <VerticalBenefitsList benefitsData={benefitsData} categoryLabel={categoryLabel} />;
+};
