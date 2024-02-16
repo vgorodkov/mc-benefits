1e12362576c915995407afdd75862f63c3cfd740
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-02-16 15:36:58 +0300

    feat: create main screen

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,13 +1,39 @@
-import { StyleSheet, View } from 'react-native';
-import React from 'react';
-import { Text } from '@components/common/Text';
+import { FlatList } from 'react-native';
+import React, { useRef } from 'react';
+
+import { benefits } from 'data/benefits';
+import { Category } from '@customTypes/benefits';
+
+import { BenefitsByCategoryList } from '@components/benefits/lists/BenefitsByCategoryList';
+import { NewBenefitsList } from '@components/benefits/lists/NewBenefitsList';
+import { useFocusEffect, useScrollToTop } from '@react-navigation/native';
+import { useDispatch } from 'react-redux';
+import { selectActiveCategory } from 'redux/slices/categorySlice';
 
 export const MainScreen = () => {
+  const benefitsByCategory = benefits.categories;
+
+  const dispatch = useDispatch();
+
+  const flatListRef = useRef<FlatList>(null);
+
+  useScrollToTop(flatListRef);
+
+  useFocusEffect(() => {
+    dispatch(selectActiveCategory(0));
+  });
+
+  const renderItem = ({ item }: { item: Category }) => {
+    return <BenefitsByCategoryList category={item} />;
+  };
+
   return (
-    <View>
-      <Text variant="body-sb">MainScreen</Text>
-    </View>
+    <FlatList
+      ref={flatListRef}
+      data={benefitsByCategory}
+      renderItem={renderItem}
+      contentContainerStyle={{ gap: 32, paddingVertical: 24 }}
+      ListHeaderComponent={() => <NewBenefitsList />}
+    />
   );
 };
-
-const styles = StyleSheet.create({});
